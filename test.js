f35ff6ac3bb2b069e3c88e405f2172aadc398f45
mathuramg <mmg9291@gmail.com>
2016-04-22 14:03:58 -0400

    initial commit

---
+++ test.js
@@ -0,0 +1,15 @@
+var JSONData =
+{
+  
+  "description": "",
+  "title": "Police are killing black people at persistently high rates.",
+  "img-url" : "http://static1.squarespace.com/static/54ecf211e4b0ed744420c5b6/t/	57114501b654f9ca26514ef6/1460749577385/PoliceKillingsTrendline.png?format=1000w",
+  "tags": [
+    "police killings",
+    "trend"
+  ],
+  "questions" : [
+    "question 1",
+    "question 2"
+  ]
+}
